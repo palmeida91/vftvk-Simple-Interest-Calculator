b8fd76ca7d094676bb319056af9e5689b1d7c17a
Paulo Almeida <paulo.vitor102@gmail.com>
2022-02-02 19:48:15 +0000

    IBM final project introduction to HTML CSS and JS

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+console.log("hello");
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal<1) {
+        alert("Please enter a positive number!");
+        document.getElementById("principal").focus(); //turn the focus to the principal element
+        return; //end the function before the calculations
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var result = document.getElementById("result");
+    //using <mark> to highlight the numbers
+    result.innerHTML = "If you deposit <mark>" + principal +"</mark>\n"+"at an interest rate of <mark>"+rate+"%</mark>.\n"+"You will"+
+    " receive an amount of <mark>"+interest+"</mark>, \nin the year <mark>"+year+"</mark>.";
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval + "%";
+    console.log("hello3");
 }
         
